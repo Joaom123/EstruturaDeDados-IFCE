42a602310c694407c9b1cda67aa526645ea3ac81
João Marcus <joaomarcus12769@gmail.com>
2019-02-11 01:52:54 -0300

    Alteraçoes no 3.c - Resoluçao do problema

--- Lista 01/3.c
+++ Lista 01/3.c
@@ -8,9 +8,60 @@ tem que ser igual ao número de linhas de B.
 #include<stdio.h>
 #include<stdlib.h>
 
+#define MATRIZ_A_LINHAS 5
+#define MATRIZ_A_COLUNAS 4
+#define MATRIZ_B_LINHAS 4
+#define MATRIZ_B_COLUNAS 3
+
 int main(int argc, char const *argv[])
 {
-    int matrizA[5][4];
-    int matrizB[4][3];
+    int matrizA[5][4] = { 
+        {1,0,0,0},
+        {1,6,7,8},
+        {1,2,3,4},
+        {1,2,3,4},
+        {1,6,7,8}
+    };
+    int matrizB[4][3]= {
+        {1,2,3},
+        {0,6,7},
+        {0,2,3},
+        {0,6,7}
+    };
+    int matrizC[5][3];
+
+
+    //Verifica se é possível
+    if (MATRIZ_A_COLUNAS == MATRIZ_B_LINHAS) {
+        int linha = 0;
+        int coluna = 0;
+        int aux;
+        int i;
+        //Multiplica as matrizes matrizA x matrizB
+
+        for( linha = 0; linha < MATRIZ_A_LINHAS; linha++)
+        {
+            for( coluna = 0; coluna < MATRIZ_B_COLUNAS; coluna++)
+            {
+                aux = 0;
+                for( i = 0; i < MATRIZ_B_LINHAS; i++)
+                {
+                    aux += matrizA[linha][i] * matrizB[i][coluna];
+                    matrizC[linha][coluna] = aux;
+                }
+            }
+            
+        }
+       
+       
+        //Imprimir a matrizC
+        for(linha=0; linha<MATRIZ_A_LINHAS; linha++){ 
+            for(coluna=0; coluna<MATRIZ_B_COLUNAS; coluna++) 
+            printf("%d ", matrizC[linha][coluna]); 
+            printf("\n"); 
+         }
+        
+    }
+
     return 0;
 }
